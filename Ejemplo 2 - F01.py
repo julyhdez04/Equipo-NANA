92fe43b0717b1352d1db308768f61745e99636cb
Hiukilll <zs23013957@estudiantes.uv.mx>
2025-03-03 21:04:47 -0600

    Ejemplo 2 - F01.py

--- Ejemplo 2 - F01.py
+++ Ejemplo 2 - F01.py
@@ -1,7 +1,11 @@
 #Equipo:  
 #Trabajaron: 
 # 2025/03/03 	V.1.0.0
-#Lo que hace este código es que a partir de una lista (en este caso de frutas) se le a
+#Lo que hace este código es que a partir de una lista (en este caso de frutas) se le añadirán más
+# elementos (más frutas) para al final imprimir la lista con los añadidos y otra impresión de cada 
+# elemento de la lista en individual
+
+
 frutas = ["manzana", "plátano", "naranja"]
 
 
