c1606d8c26762b26899c646b628b0f43f7d404e3
Pinging78 <143364629+Pinging78@users.noreply.github.com>
2025-03-03 21:06:47 -0600

    Update Ejemplo 1 - F01.py

--- Ejemplo 1 - F01.py
+++ Ejemplo 1 - F01.py
@@ -1,6 +1,6 @@
-#Equipo: diablo
+#Equipo: Nana
 #Trabajaron:Josue Marcelo y Kaylee Michelle
-# 2025/03/03 	V.1.0.0
+# 2025/03/03 	V.1.0.1
 # El siguiente código realiza las 4 operaciones basicas con dos numeros dados su finalidad es mostrar los
 # resultados al usuario 
 numero1 = 5  #Asigna un valor a una variable con ayuda del signo igual
@@ -10,11 +10,12 @@ numero2 = 3  #Igual asignamos valor a la variable
 
 suma = numero1 + numero2 #Guarda en la variable llamada "suma" la operación sumatoria de las primeras variables 
 resta = numero1 - numero2 #Guarda en la variable llamada "resta" la operación resta de las primeras variables    
-multiplicacion = numero1 * numero2
-division = numero1 / numero2
+multiplicacion = numero1 * numero2 #Guarda la variable llamada "multiplicacion" esta operación multiplicara las variables asignadas
+division = numero1 / numero2 #Guarda la variable llamada "division" y dividira las variables asignadas
 
+#Ahora en esta seccion se va a seleccionar que operacion se quiere hacer
 
-print("Suma:", suma)
-print("Resta:", resta)
-print("Multiplicación:", multiplicacion)
-print("División:", division)
+print("Suma:", suma) #Se muestra el resultado de la Suma de las variables asignadas en numero1 y numero2
+print("Resta:", resta) #Se muestra el resultado de la Resta de las variables asignadas en numero1 y numero2
+print("Multiplicación:", multiplicacion) #Se muestra el resultado de la Multiplicación de las variables asignadas en numero1 y numero2
+print("División:", division) #Se muestra el resultado de la División de las variables asignadas en numero1 y numero2
