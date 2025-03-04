fcbcc81218a375da0dad52c5ddef064ee5007d75
Hiukilll <zs23013957@estudiantes.uv.mx>
2025-03-03 20:58:08 -0600

    Ejemplo 2 - F01.py

--- Ejemplo 2 - F01.py
+++ Ejemplo 2 - F01.py
@@ -1,6 +1,7 @@
 #Equipo:  
 #Trabajaron: 
 # 2025/03/03 	V.1.0.0
+#Lo que hace este código es que a partir de una lista (en este caso de frutas) se le a
 frutas = ["manzana", "plátano", "naranja"]
 
 
