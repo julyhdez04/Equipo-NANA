4399536db216b01cc5bb553756518bd78917519a
Choclotherock <zs23013943@estudiantes.uv.mx>
2025-03-03 21:23:06 -0600

    V.1.0.2 Ejemplo 3 - F01.py
    Pues le agregue para que sirve todo el codigo y asi -Choclotherock

--- Ejemplo 3 - F01.py
+++ Ejemplo 3 - F01.py
@@ -1,9 +1,12 @@
 #Equipo: 
-#Trabajaron: Juliana Hernández
-# 2025/03/03 	V.1.0.1
+#Trabajaron: Juliana Hernández, Cesar David (choclotherock)
+# 2025/03/03 	V.1.0.2
 numero = int(input("Ingrese un número: ")) #Guarda la variable "número" como un dato de entrada de valor entero, el cuál el usuario debe ingresar, "input" es la función que lee el dato ingresado.
 
 if numero % 2 == 0: #Condicional para ejecutar las siguientes líneas, refiere a que si el residuo de un número dividido entre 2 es igual a 0, entonces...
     print("El número es par") #...imprimir "El número es par"
 else:  #Sino...
     print("El número es impar") #Imprimir que el número es impar.
+
+#pues como pueden ver asi de que hace cada funcion por separado pues es que cuando le ingresemos un numerito la compu va aser una division entre 2 y la cosa es que si suelta un residuo pues es que no es par y si no lo hace pues awebo que es par padrino -Choclotherock
+#Lalalalala -Choclotherock
