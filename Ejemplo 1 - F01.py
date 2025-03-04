fabd52d8bf36364f91fdb780c567609540ee5237
kay16mich <ZS23013918@estudiantes.uv.mx>
2025-03-03 20:44:23 -0600

    Update Ejemplo 1 - F01.py
    Casi la mitad

--- Ejemplo 1 - F01.py
+++ Ejemplo 1 - F01.py
@@ -1,5 +1,5 @@
-#Equipo: 
-#Trabajaron: 
+#Equipo: Nana
+#Trabajaron: Hugo de Jesus, Josue Marcelo, Juliana, Kaylee Michelle y Choclo
 # 2025/03/03 	V.1.0.0
 # El siguiente código realiza las 4 operaciones basicas con dos numeros dados su finalidad es mostrar los
 # resultados al usuario 
@@ -9,7 +9,7 @@ numero2 = 3  #Igual asignamos valor a la variable
 # Con el fin de trabajar comodamente con las cantidades se guardan en variables con nombres fáciles de recordar para el que las maneje 
 
 suma = numero1 + numero2 #Guarda en la variable llamada "suma" la operación sumatoria de las primeras variables 
-resta = numero1 - numero2 #
+resta = numero1 - numero2 #Guarda en la variable llamada "resta" la operación resta de las primeras variables    
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
 
