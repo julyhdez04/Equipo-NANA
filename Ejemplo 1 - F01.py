11d67589103bb04eb8d53df32b249fddaaea01a5
kay16mich <ZS23013918@estudiantes.uv.mx>
2025-03-03 20:36:06 -0600

    Add files via upload

---
+++ Ejemplo 1 - F01.py
@@ -0,0 +1,20 @@
+#Equipo: 
+#Trabajaron: 
+# 2025/03/03 	V.1.0.0
+# El siguiente código realiza las 4 operaciones basicas con dos numeros dados su finalidad es mostrar los
+# resultados al usuario 
+numero1 = 5  #Asigna un valor a una variable con ayuda del signo igual
+numero2 = 3  #Igual asignamos valor a la variable
+
+# Con el fin de trabajar comodamente con las cantidades se guardan en variables con nombres fáciles de recordar para el que las maneje 
+
+suma = numero1 + numero2 #Guarda en la variable llamada "suma" la operación sumatoria de las primeras variables 
+resta = numero1 - numero2 #
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
