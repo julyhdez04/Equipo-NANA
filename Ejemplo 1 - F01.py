fec00a9b67a1af857b0af0c9db70104f127505aa
kay16mich <ZS23013918@estudiantes.uv.mx>
2025-03-03 21:41:55 -0600

    Update Ejemplo 1 - F01.py
    Agrego sintaxis y ejemplo al final del codigo

--- Ejemplo 1 - F01.py
+++ Ejemplo 1 - F01.py
@@ -1,6 +1,6 @@
 #Equipo: Nana
 #Trabajaron:Josue Marcelo y Kaylee Michelle
-# 2025/03/03 	V.1.0.1
+# 2025/03/03 	V.1.0.2
 # El siguiente código realiza las 4 operaciones basicas con dos numeros dados su finalidad es mostrar los
 # resultados al usuario 
 numero1 = 5  #Asigna un valor a una variable con ayuda del signo igual
@@ -19,3 +19,17 @@ print("Suma:", suma) #Se muestra el resultado de la Suma de las variables asigna
 print("Resta:", resta) #Se muestra el resultado de la Resta de las variables asignadas en numero1 y numero2
 print("Multiplicación:", multiplicacion) #Se muestra el resultado de la Multiplicación de las variables asignadas en numero1 y numero2
 print("División:", division) #Se muestra el resultado de la División de las variables asignadas en numero1 y numero2
+
+# En esta ultima sección se muestran al usuario los resultados de las operaciones elaboradas.
+
+#Sintaxis de función "print()": 
+# print("texto", resultado)
+# En este caso expongo esta sintaxis ya que es la ocupada en el codigo anterior . Se ocupa para poder mostrar/ imprimir en la pantalla lo que 
+# colocamos dentro del parentesis siguiente a llamado, entre comillas se coloca un texto tipo preterminado por el programador que quiere que vea el usuario.
+# es una función muy versatil porque podemos imprimir texto, numeros, resultados,etc. En el caso del texto siguiente separado por una coma y sin comillas dentro del parentesis
+# llevamos a la funcion a imprimir un resultado en este caso el valor asignado a una variable.
+
+# Un pequeño ejemplo: 
+# var1 = "Esto es un ejemplo"
+# print("Muestro tu escrito",cadena)
+# Muestro tu escrito Esto es un ejemplo
