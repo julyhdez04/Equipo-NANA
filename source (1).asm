d2d4659c85d05a28b3568d688f240efa711ed48a
kay16mich <ZS23013918@estudiantes.uv.mx>
2025-03-05 18:10:10 -0600

    Update source (1).asm
    Comenté hasta el primer bucle

--- source (1).asm
+++ source (1).asm
@@ -1,27 +1,33 @@
 ; Example 1.1:
 ; Writes "Hello World!" to the text display
+;Versión 1.1.0 
 
-	JMP boot
+	JMP boot	;Con la función JMP realizamos un salto a la posición que le indiquemos en el argumento
+			;en este caso hasta la zona de "boot" con el fin de evitar que lea las definiciones que
+			;se realicen antes de dicha zona especificada.
 
-stackTop    EQU 0xFF    ; Initial SP
-txtDisplay  EQU 0x2E0
+stackTop    EQU 0xFF    ;EQU significa equivalente a algo, 0x definimos que lo siguiente se encuentra en el sistema
+			;hexadecimal en este caso se refiere a la posición que queremos que inicie el puntero de pila
+			;en la linea queremos que inice en el punto más alto de la memoria
+txtDisplay  EQU 0x2E0	;Tambien definimos una ubicación pero en este caso es del texto
 
-hello:	DB "(*-*)"	; Output string
-		DB 0				; String terminator
-		DB "(*0*)"
+caritas:	DB "(*-*)"	;caritas almacena como una variable todo lo que queremos imprimir
+		DB 0		;DB definimos el byte 
+		DB "(*0*)"	;Nuesto DB0 lo tomamos como nulo/espacio/vacío/terminado
 		DB 0
-        DB "(*~*)"
-        DB 0
-boot:
-	MOV SP, stackTop	; Set SP
-	MOV C, hello		; Point register C to string
-	MOV D, txtDisplay	; Point register D to output
-	CALL print
-	HLT				; Halt execution
+       	 	DB "(*~*)"
+        	DB 0
 
-print:				; Print string
+boot:				;Aquí ya incia a leer
+	MOV SP, stackTop	;Inicia el puntero de pila en la dirección indicada 
+	MOV C, caritas		;Apunta el registro c al inicio del texto al primer caracter del mesaje
+	MOV D, txtDisplay	;Apunta el registro d a la dirección donde se imprime el primer texto
+	CALL print		;CALL es igual a llamar la función en este caso imprimir 
+	HLT			;Detiene la ejecución despues de la impresion primera pero nuestro codigo reinicia el ciclo
 
-.loop:
+print:				;Con la funcion print imprimimos la cadena y pasamos al ciclo
+
+.loop:				;Genera un ciclo dentro del código
 	MOVB AL, [C]	; Get character
 	MOVB [D], AL	; Write to output
 	INC C
