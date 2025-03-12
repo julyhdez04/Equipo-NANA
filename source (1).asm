6a70ce5c19bb7fd06f1bf4624eebcc4e960c970a
julyhdez04 <143435308+julyhdez04@users.noreply.github.com>
2025-03-12 15:33:31 -0600

    Update source (1).asm
    Se agregan comentarios explicativos V 2. 0. 1

--- source (1).asm
+++ source (1).asm
@@ -1,14 +1,15 @@
-;Versión 2.0.0
+;Versión 2.0.1
 
-	JMP boot
-stackTop EQU 0x02DF
-texto EQU 0x2E0	
-pantalla EQU 0x300
-findelapantalla EQU 0x400
-pantalla2 EQU 0x350
+	JMP boot ;Saltar al inicio del programa
+stackTop EQU 0x02DF ;Definir el puntero con posición hexadecimal
+texto EQU 0x2E0	 ;Definir etiqueta de texto en celda con posición hexadecimal
+pantalla EQU 0x300 Definir etiqueta de display en celda con posición hexadecimal
+findelapantalla EQU 0x400 ;Definir tamaño del display (final) en celda con posición hexadecimal
+pantalla2 EQU 0x350 ;Definir etiqueta de segundo display en celda con posición hexadecimal
 gatos:
-DB "Miau miau miau  miau miau mew :3"
-        DB 0
+DB "Miau miau miau  miau miau mew :3" ;Definir byte de texto a mostrar
+        DB 0 ;Finalizar texto
+;Definir bytes para la animación con bytes de colores
 DB "\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4"
 DB "\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4"
 DB "\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4"
@@ -66,37 +67,37 @@ DB "\xFF\xFF\xC4\xC4\xC4\xC4\xC4\xC4"
         	DB 0
 delay EQU 1
 
-boot:				
-	MOV SP, stackTop	 
-	MOV C, gatos
-	MOV D, texto
-	CALL .loop		
-	HLT			
+boot:	;Inicio de ejecución del programa			
+	MOV SP, stackTop ;Mover SP a la posición del puntero	 
+	MOV C, gatos ;Mover C a posición definida en "gatos"
+	MOV D, texto ;Mover D a posición definida en "texto"
+	CALL .loop ;Llamar la función loop (bucle) para su ejecución		
+	HLT ;Finalizar programa		
 
-.loop:				
-	MOVB AL, [C]	
-	MOVB [D], AL	
-	INC C
-	INC D
-	CMPB BL, [C]	
-	JNZ .loop		
-	MOV D, pantalla
-    INC C
-    CALL .loop2	
-    CALL boot
-    RET
+.loop:		;Inicia el bucle		
+	MOVB AL, [C]	;Sobreescribir "AL" en la posición de C
+	MOVB [D], AL	;Sobreescribir D en la posición de AL de la línea anterior
+	INC C ;Incrementar C para mover la posición
+	INC D ;Incrementar D para mover la posición
+	CMPB BL, [C] ;Comparar el valor de BL con C
+	JNZ .loop ;Condicional, si no se cumple la comparación, retornar a loop, si si se cumple, continuar	
+	MOV D, pantalla ;Mover D a la posición establecida en "pantalla"
+    INC C ;Incrementar C para mover la posición
+    CALL .loop2	;Llamar la función loop2
+    CALL boot ;Llamar la función boot
+    RET ;Retornar
    
 .loop2:
-	MOVB AL, [C]	
-	MOVB [D], AL	
-	INC C
-	INC D
-	CMP D,findelapantalla
-    JNZ .loop2
-    MOV D, delay
-    INC C
-    CALL .loopDELAY
-    RET
+	MOVB AL, [C] ;Mover AL a la posición de C	
+	MOVB [D], AL ;Mover D a la posición de AL
+	INC C ;Incrementar C para mover la posición
+	INC D;Incrementar D para mover la posición
+	CMP D,findelapantalla ;Comparar D  on "findelapantalla" si no se cumple, retornar
+    JNZ .loop2 ;Regresar a loop2 si no se cumple la condición anterior
+    MOV D, delay ;Mover D a la posición de "delay"
+    INC C ;Incrementar C para mover la posición
+    CALL .loopDELAY ;Llamar a la función de delay del bucle
+    RET ;Retornar
     
 .loopDELAY:
     INC B
