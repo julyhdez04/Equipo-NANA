e77ccf6a95cfe5f6bf4f67492ec5f39d0ef46846
Choclotherock <zs23013943@estudiantes.uv.mx>
2025-03-11 19:52:24 -0600

    Agregar la funcion Delay
    Aca agregamos la funcion de Delay y asi, quitamos la parte de print pq no servia pa nada en lo que andamos haciendo je, y cambie las caritas para poder entender mejor los cambios -Choclotherock

--- source (1).asm
+++ source (1).asm
@@ -1,6 +1,4 @@
-; Example 1.1:
-; Writes "Hello World!" to the text display
-;Versión 1.1.0 
+;Versión 1.2.0 
 
 	JMP boot	;Con la función JMP realizamos un salto a la posición que le indiquemos en el argumento
 			;en este caso hasta la zona de "boot" con el fin de evitar que lea las definiciones que
@@ -11,29 +9,31 @@ stackTop    EQU 0xFF    ;EQU significa equivalente a algo, 0x definimos que lo s
 			;en la linea queremos que inice en el punto más alto de la memoria
 txtDisplay  EQU 0x2E0	;Tambien definimos una ubicación pero en este caso es del texto
 
-caritas:	DB "(*-*)"	;caritas almacena como una variable todo lo que queremos imprimir
+caritas:
+		DB "Ola profe aquí los mas cabrones."
+        DB 0
+		DB "++++"	;caritas almacena como una variable todo lo que queremos imprimir
 		DB 0		;DB definimos el byte 
-		DB "(*0*)"	;Nuesto DB0 lo tomamos como nulo/espacio/vacío/terminado
+		DB "////"	;Nuesto DB0 lo tomamos como nulo/espacio/vacío/terminado
 		DB 0
-       	 	DB "(*~*)"
+       	 	DB "----"
         	DB 0
+delay EQU 250
 
 boot:				;Aquí ya incia a leer
 	MOV SP, stackTop	;Inicia el puntero de pila en la dirección indicada 
 	MOV C, caritas		;Apunta el registro c al inicio del texto al primer caracter del mesaje
 	MOV D, txtDisplay	;Apunta el registro d a la dirección donde se imprime el primer texto
-	CALL print		;CALL es igual a llamar la función en este caso imprimir 
+	CALL .loop		;CALL es igual a llamar la función en este caso imprimir 
 	HLT			;Detiene la ejecución despues de la impresion primera pero nuestro codigo reinicia el ciclo
 
-print:				;Con la funcion print imprimimos la cadena y pasamos al ciclo
-
 .loop:				;Genera un ciclo dentro del código
-	MOVB AL, [C]	; Get character
-	MOVB [D], AL	; Write to output
+	MOVB AL, [C]	
+	MOVB [D], AL	
 	INC C
 	INC D
-	CMPB BL, [C]	; Check if string terminator
-	JNZ .loop		; Jump back to loop if not
+	CMPB BL, [C]	
+	JNZ .loop		
 	MOV D, txtDisplay
     INC C
     CALL .loop2	
@@ -41,16 +41,26 @@ print:				;Con la funcion print imprimimos la cadena y pasamos al ciclo
     RET
    
 .loop2:
-	MOVB AL, [C]	; Get character
-	MOVB [D], AL	; Write to output
+	MOVB AL, [C]	
+	MOVB [D], AL	
 	INC C
 	INC D
 	CMPB BL, [C]	
     JNZ .loop2
-    MOV D, txtDisplay
+    MOV D, delay
     INC C
+    CALL .loopDELAY
+    RET
+    
+.loopDELAY:
+    INC B
+    CMP B, D
+    JNZ .loopDELAY
+    MOV D, txtDisplay
+    MOV B, 0
     CALL .loop22
     RET
+
 .loop22:
 	MOVB AL, [C]
     MOVB [D], AL
