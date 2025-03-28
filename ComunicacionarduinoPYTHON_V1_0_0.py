fdde994dd2f186882c7640396f8fc817b8e18258
Choclotherock <zs23013943@estudiantes.uv.mx>
2025-03-27 22:37:37 -0600

    PYTHON arduino v.1.1.0
    Se agregan mayor numero de sensores que hay que imprimir, y se prueban diferentes formas de imprimirlo

--- ComunicacionarduinoPYTHON_V1_0_0.py
+++ ComunicacionarduinoPYTHON_V1_0_0.py
@@ -1,18 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 25 22:20:09 2025
+V 1.1.0
+@author: Chocl
+"""
 import serial
 import time
 
 # Configura el puerto serie (cambia 'COM4' por tu puerto Arduino)
-arduino = serial.Serial ('COM4', 9600, timeout=1)
+arduino = serial.Serial ('COM3', 9600, timeout=1)
 
 try:
     while True:
         if arduino.in_waiting > 0:
             linea = arduino.readline().decode('utf-8').strip()
             
-            if linea.startswith("TEMP:"):
+            if linea.startswith("Locura3:"):
+                temperatura = linea  # Extrae el valor numérico
+                print(f"primersensor: {temperatura} °C")
+            if linea.startswith("Locura4:"):
                 temperatura = float(linea.split(":")[1])  # Extrae el valor numérico
-                print(f"Temperatura recibida: {temperatura} °C")
+                print(f"segundosensor: {temperatura} °C")
 
+                
 except KeyboardInterrupt:
     print("Interrupción por usuario. Cerrando conexión.")
     arduino.close()
+    
