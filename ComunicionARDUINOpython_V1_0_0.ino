0ffd6a7c82e800af2b4248510cdb1904937017c8
julyhdez04 <143435308+julyhdez04@users.noreply.github.com>
2025-03-28 21:12:41 -0600

    Update ComunicionARDUINOpython_V1_0_0.ino
    V.1.2.1 Se añaden comentarios

--- ComunicionARDUINOpython_V1_0_0.ino
+++ ComunicionARDUINOpython_V1_0_0.ino
@@ -1,17 +1,17 @@
 //V.1.2.0
 
-#include <Ultrasonic.h>
-const int TRIG = 8;
-const int ECHO = 10;
-int distance;
-Ultrasonic ultrasonic(TRIG, ECHO);
+#include <Ultrasonic.h> //Incluir libreria para sensor ultrasónico 
+const int TRIG = 8; //Definir pin de conexión TRIG del sensor
+const int ECHO = 10; //Definir pin de conexion ECHO del sensor
+int distance; //Variable para distancia 
+Ultrasonic ultrasonic(TRIG, ECHO); //Definir conexiones a usar en el sensor
 
 
 
-#include <DHT.h>
-#define DHTPIN 2     // Pin donde está conectado el DHT11
+#include <DHT.h> //Incluir libreria para el sensor de temperatura
+#define DHTPIN 2      //Pin donde está conectado el DHT11
 #define DHTTYPE DHT11   // Definimos el tipo de sensor DHT
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); //Definir conexiones del sensor
 
 
 
@@ -29,33 +29,33 @@ void setup() {
 }
 
 void loop() {
-  delay(2000);
+  delay(2000); //Retraso de 2000 ms para el loop
 
 // SENSOR DE HUMEDAD Y TEMPERATURA
   float h = dht.readHumidity();    // Lee la humedad y la temperatura
   float t = dht.readTemperature(); // En grados Celsius
   if (isnan(h) || isnan(t)) {   // Verifica si hay errores en la lectura
-    Serial.println("Error al leer del sensor DHT");
+    Serial.println("Error al leer del sensor DHT"); //Imprime mensaje de erros si no logra dar lectura al sensor
     return;
   }
   Serial.print("Humedad: ");     // Envía los datos a través del puerto serial
-  Serial.println(h);
-  Serial.print("Temperatura: ");
-  Serial.println(t);
+  Serial.println(h); //Imprime valor de Humedad
+  Serial.print("Temperatura: "); //Envia una cadena de texto al monitor serial
+  Serial.println(t); //Imprime el dato de temperatura al monitor serial
 
 //SENSOR DE MOVIMIENTO
   val = digitalRead(PIRpin);  // Lee el valor del sensor PIR
-  Serial.print("MOVIMIENTO: "); 
-  if (val==0){
+  Serial.print("MOVIMIENTO: ");  //Imprime texto en el lonitor serial
+  if (val==0){ //Condicional si el valor es igual a 0...
 
-  Serial.println(val);
+  Serial.println(val); //...imprimir la variable
   }
     else
 
-      Serial.println(val);
+      Serial.println(val); //Sino imprimir el valor obtenido 
   
 //SENSOR DE PROXIMIDAD
-  distance = ultrasonic.read();
-  Serial.print("Distancia: ");
+  distance = ultrasonic.read(); //Definir dato de distancia a traves de la lectura del sensor
+  Serial.print("Distancia: "); //Imprimir texto 
   Serial.println(distance);      // Imprimir la distancia en el monitor serial
 }
