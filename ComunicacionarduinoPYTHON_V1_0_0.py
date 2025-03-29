0e53de4fe84965c7a63ade6284a6f07423990888
Choclotherock <zs23013943@estudiantes.uv.mx>
2025-03-28 18:01:30 -0600

    ComunicacionarduinoPYTHON_V1_1_0.py
    Se agregan cambios para el aumento de recepccion de datos
    -Choclotherock

--- ComunicacionarduinoPYTHON_V1_0_0.py
+++ ComunicacionarduinoPYTHON_V1_0_0.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 25 22:20:09 2025
-V 1.1.0
-@author: Chocl
+
+@author: Choclotherock
+         Pinging78
+         kay16mich
+         julyhdez04
 """
 import serial
-import time
 
 # Configura el puerto serie (cambia 'COM4' por tu puerto Arduino)
-arduino = serial.Serial ('COM3', 9600, timeout=1)
+arduino = serial.Serial ('COM4', 9600, timeout=1)
 
 try:
     while True:
         if arduino.in_waiting > 0:
             linea = arduino.readline().decode('utf-8').strip()
-            
-            if linea.startswith("Locura3:"):
-                temperatura = linea  # Extrae el valor numérico
-                print(f"primersensor: {temperatura} °C")
-            if linea.startswith("Locura4:"):
-                temperatura = float(linea.split(":")[1])  # Extrae el valor numérico
-                print(f"segundosensor: {temperatura} °C")
+
+            if linea.startswith("Humedad: "):
+                hume = float(linea.split(":")[1])  # Extrae el valor numérico
+                print(f"Humedad: {hume}")
+                
+            if linea.startswith("Temperatura: "):
+                    temp = float(linea.split(":")[1])  # Extrae el valor numérico
+                    print(f"Temperatura: {temp} °C")
+                    
+            if linea.startswith("MOVIMIENTO: "):
+                    mov = float(linea.split(":")[1])  # Extrae el valor numérico
+                    print(f"MOVIMIENTO: {mov}")
+                    
+            if linea.startswith("Distancia: "):
+                    dis = float(linea.split(":")[1])  # Extrae el valor numérico
+                    print(f"Distancia: {dis}")
 
                 
 except KeyboardInterrupt:
